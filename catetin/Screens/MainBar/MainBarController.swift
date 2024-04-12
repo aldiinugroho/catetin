a4a685c7053bf4fbb0782a9fefae854f749b3be6
aldi nugroho <nugroho.aaldi@gmail.com>
2024-04-12 17:18:35 +0700

    update

--- catetin/Screens/MainBar/MainBarController.swift
+++ catetin/Screens/MainBar/MainBarController.swift
@@ -11,16 +11,18 @@ import AsyncDisplayKit
 class MainBarController: ASTabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let view1: ASDKViewController<ASDisplayNode> = MainViewController()
-        view1.title = "Home"
-        let view2: ASDKViewController<ASDisplayNode> = ASDKViewController()
-        view2.view.backgroundColor = .baseGray
-        view2.title = "Account"
+        let view1: ASDKViewController<ASDisplayNode> = setupBar(view: MainViewController(), title: "Home")
+        let view2: ASDKViewController<ASDisplayNode> = setupBar(view: ASDKViewController(), title: "Account")
         self.tabBar.backgroundColor = .white
         self.tabBar.tintColor = .black
         self.setViewControllers([
             view1,
             view2
         ], animated: false)
+    }
+    
+    func setupBar(view: ASDKViewController<ASDisplayNode>, title: String? = "") -> ASDKViewController<ASDisplayNode> {
+        view.title = title
+        return view
     }
 }
